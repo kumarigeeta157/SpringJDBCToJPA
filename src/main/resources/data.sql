d2c448b58176dfe0dcb0b8752b7ef0023e3cbba1
kumarigeeta157 <kmgeeta157@gmail.com>
2023-05-29 17:57:02 +0530

    this is second commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,15 @@ id integer not null,
 name varchar(30) not null,
 location varchar(30) not null
 );
+
+
+INSERT INTO PERSON(ID,NAME,LOCATION)
+VALUES(1001, 'akash', 'AGRA' );
+
+INSERT INTO PERSON(ID,NAME,LOCATION)
+VALUES(1002, 'ashish', 'Aliganj' );
+
+INSERT INTO PERSON(ID,NAME,LOCATION)
+VALUES(1003, 'aman', 'kasganj' );
+
 select * from person;
